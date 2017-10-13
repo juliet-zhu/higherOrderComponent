7d1041cab49cb0f32efd66aa46ca7feb291d8a5b
juliet-zhu <juliet.zhu.work@gmail.com>
2017-10-13 11:23:03 +0800

    init project

---
+++ src/components/resources.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            Super Special Secret Recipe
+            <li>1 cup suger</li>
+            <li>1 cup pepper</li>
+            <li>1 cup salt</li>
+        </div>
+    )
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import authentication from './authentication'
+
+const rootReducer = combineReducers({
+  authenticated:authentication
+});
+
+export default rootReducer;
